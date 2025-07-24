c8b86b39ba4cd9d57db8c3a2cde357580c29ec0b
Diana Amimo <amimodiana9@gmail.com>
2025-07-24 08:50:26 +0300

    initial commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,8 +1,12 @@
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20" px-6>
+        <HeroSection />
+      </div>
     </>
   );
 };
--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -37,8 +37,27 @@ const Navbar = () => {
                     {mobileDrawerOpen ? <X /> : <Menu />}
                 </button>
             </div>
-            
         </div>
+        {mobileDrawerOpen && (
+          <div className=".fixed.right-0. z-20.bg-neutral-900.w-full.p-12.flex.flex-col.justify.items-center.lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+              <li key={index} className="py-4">
+                <a href="{item.href}">{item.label}</a>
+              </li>
+
+              ))}
+            </ul>
+            <div className="flex.space-x-6" >
+              <a href="#" class="py-2 px-3 border rounded-md">
+                Sign In
+              </a>
+              <a href="#"className="py-2 px-3 rounded-md gradient-to-r from-orange-500 to-orange-800">
+                Create an account
+              </a>
+            </div>
+          </div>
+        )}
     </div>
   </nav>
     
